b1b503b4f8a76a5c822977879cb95401d4cdeb16
darrelhong <darrel.hong@gmail.com>
2021-03-27 17:01:36 +0800

    show events in edge server

--- edge_server/edge.py
+++ edge_server/edge.py
@@ -3,15 +3,21 @@ from flask import Blueprint, render_template
 from edge_server.db import query_db, get_db
 
 # all routes are prefixed with /api
-edge = Blueprint('edge', __name__)
+edge = Blueprint("edge", __name__)
 
 
-@edge.route('/hello')
+@edge.route("/hello")
 def hello():
-    return 'Hello API'
+    return "Hello API"
 
 
-@edge.route('/')
+@edge.route("/")
 def index():
     data = query_db("SELECT * FROM edge")
     return render_template("index.html", data=data)
+
+
+@edge.route("/events")
+def events():
+    data = query_db("SELECT * FROM events")
+    return render_template("events.html", data=data)
--- edge_server/edge_schema.sql
+++ edge_server/edge_schema.sql
@@ -1,4 +1,6 @@
 DROP TABLE IF EXISTS edge;
+DROP TABLE IF EXISTS events;
+
 
 --implicit rowid
 CREATE TABLE edge (
@@ -8,3 +10,9 @@ CREATE TABLE edge (
   sent_to_cloud INTEGER DEFAULT 0 CHECK(sent_to_cloud = 0 or sent_to_cloud = 1) NOT NULL,
   time_recorded TIMESTAMP DEFAULT (datetime('now','localtime')) NOT NULL
 );
+
+CREATE table events (
+  station TEXT NOT NULL,
+  event_name TEXT NOT NULL,
+  time_recorded TIMESTAMP DEFAULT (datetime('now','localtime')) NOT NULL
+)
